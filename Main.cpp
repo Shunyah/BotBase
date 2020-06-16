1730d35c5485941b0244f6700f76c03bda419d41
shunyah <ammerlie17@gmail.com>
2020-06-16 13:52:18 +0300

    init

---
+++ Main.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <ViZDoom.h>
+
+
+using namespace vizdoom;
+using namespace cv;
+using namespace std;
+
+
+int main()
+{
+	cout << "Hello World!" << endl;
+}
